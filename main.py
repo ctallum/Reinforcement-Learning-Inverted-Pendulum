329462c789737e7d8bac55af423facee977a5d5f
Chris Allum <ctallum@gmail.com>
2021-12-12 22:23:07 -0500

    Update main

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
-# Machine Learning Final Project
-# Reinforcement Learning Game Project - Swing Up Two Link Pendulum
-from ray.rllib.agents.ppo import PPOTrainer
-
-def main():
-    # Configure the algorithm.
-    config = {
-        # Environment (RLlib understands openAI gym registered strings).
-        "env": "CartPole-v1",
-        "num_workers": 2,
-        "framework": "torch",
-        "model": {
-            "fcnet_hiddens": [64, 64],
-            "fcnet_activation": "relu",
-            },
-        "evaluation_num_workers": 1,
-        "evaluation_config": {
-            "render_env": True,
-            }
-    }
-    # Create our RLlib Trainer.
-    trainer = PPOTrainer(config=config)
-
-    for _ in range(20):
-        print(trainer.train())
-
-    trainer.evaluate()
+import gym
+from ReinforcementLearning import Agent
+import numpy as np
 
 if __name__ == "__main__":
-    main()
+    env = gym.make('LunarLander-v2')
+    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, eps_end=0.01, input_dims=[8], learning_rate=0.003)
+    scores, eps_history = [], []
+    n_games = 50
+
+    observation = env.reset()
+    for _ in range(1000):
+        env.render()
+        action = agent.choose_action(observation)
+        observation, reward, done, info = env.step(action) 
+        if done:
+            break
+    env.close()
+
+    for game in range(n_games):
+        score = 0
+        done = False
+        observation = env.reset()
+        while not done:
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            score += reward
+            agent.store_transition(observation, action, reward, observation_, done)
+
+            agent.learn()
+            observation = observation_
+
+        scores.append(score)
+        eps_history.append(agent.epsilon)
+
+        avg_score = np.mean(scores[-100:])
+
+        print('episode', game, 'score %.2f' % score,
+                'average score %.2f' % avg_score,
+                'epsilon %.2f' % agent.epsilon)
+    
+    observation = env.reset()
+    for _ in range(10000):
+        env.render()
+        action = agent.choose_action(observation)
+        observation, reward, done, info = env.step(action) 
+        if done:
+            break
+    env.close()
