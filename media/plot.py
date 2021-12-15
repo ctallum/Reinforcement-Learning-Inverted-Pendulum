537b99d1d511b17318c931c8fecf63693d3fd200
Chris Allum <ctallum@gmail.com>
2021-12-15 02:39:37 -0500

    add graphs

--- media/plot.py
+++ media/plot.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 def plot_single(games, learning_rate):
 
     data = np.genfromtxt(f'media/csv/{games}-games-lr-{learning_rate}.csv')
-
+    plt.figure()
     plt.plot(data)
     plt.xlabel('Episodes')
     plt.ylabel('Training Score')
     plt.legend([f'Learning Rate = {learning_rate}'])
+    plt.title(f'Game results with {games} episode trainings and {learning_rate} learning rate')
     #plt.show()
     plt.savefig(f'media/{games}-games-lr-{learning_rate}.png')
 
 
-plot_single(500,0.001)
+for episode in [10, 20, 50, 100, 200, 500]:
+    for lr in [0.001, 0.0001]:
+        plot_single(episode,lr)
